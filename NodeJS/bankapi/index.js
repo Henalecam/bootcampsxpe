36b10ecc6a224e0e2f1be3fac2bb307d5d5cd740
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-02-16 20:24:35 -0300

    API 1/3 Finalizada

--- NodeJS/bankapi/index.js
+++ NodeJS/bankapi/index.js
@@ -1,9 +1,30 @@
-import { Express } from 'express';
-import accountsRouter from './routes/accounts';
+import express from 'express';
+import accountsRouter from './routes/accounts.js';
+import { promises as fs } from 'fs';
+
+const { readFile, writeFile } = fs;
 
 const app = express();
+
 app.use(express.json());
-app.use('/account'accountsRouter);
-app.listen(3000, () => {
-  console.log('API Started!');
+
+app.use('/account', accountsRouter);
+
+app.listen(3000, async () => {
+  try {
+    await readFile(global.fileName);
+    console.log('API Started!');
+  } catch (err) {
+    const initalJson = {
+      nextID: 1,
+      accounts: [],
+    };
+    writeFile(global.fileName, JSON.stringify(initalJson))
+      .then(() => {
+        console.log('API Started and File Created!');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 });
--- NodeJS/bankapi/routes/accounts.js
+++ NodeJS/bankapi/routes/accounts.js
@@ -1,4 +1,56 @@
-import { Express } from 'express';
+import express from 'express';
+import { promises as fs, read, write } from 'fs';
+
+const { readFile, writeFile } = fs;
+global.fileName = 'accounts.json';
 const router = express.Router();
+
+router.post('/', async (req, res) => {
+  try {
+    let account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    account = { id: data.nextID++, ...account };
+    data.accounts.push(account);
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    delete data.nextID;
+    res.send(data);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const account = data.accounts.find(
+      account => account.id === parseInt(req.params.id)
+    );
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      account => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.end();
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
 
 export default router;
