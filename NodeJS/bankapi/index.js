584e5c868c6c8107d87dd4e6264f58d417523b64
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-02-16 17:11:59 -0300

    sla

--- NodeJS/bankapi/index.js
+++ NodeJS/bankapi/index.js
@@ -1,7 +1,9 @@
 import { Express } from 'express';
+import accountsRouter from './routes/accounts';
 
 const app = express();
 app.use(express.json());
+app.use('/account'accountsRouter);
 app.listen(3000, () => {
   console.log('API Started!');
 });
