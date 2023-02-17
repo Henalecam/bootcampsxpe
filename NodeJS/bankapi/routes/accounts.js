17df35ace727d22b6da28b2567b2bc6f8d1a4ec9
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-02-17 20:18:01 -0300

    Next & Winston update

--- NodeJS/bankapi/routes/accounts.js
+++ NodeJS/bankapi/routes/accounts.js
@@ -5,7 +5,7 @@ const { readFile, writeFile } = fs;
 global.fileName = 'accounts.json';
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     let account = req.body;
     const data = JSON.parse(await readFile(global.fileName));
@@ -14,21 +14,21 @@ router.post('/', async (req, res) => {
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.send(account);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     delete data.nextID;
     res.send(data);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     const account = data.accounts.find(
@@ -36,11 +36,11 @@ router.get('/:id', async (req, res) => {
     );
     res.send(account);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res, next) => {
   try {
     const data = JSON.parse(await readFile(global.fileName));
     data.accounts = data.accounts.filter(
@@ -49,11 +49,11 @@ router.delete('/:id', async (req, res) => {
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
     res.end();
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.put('/', async (req, res) => {
+router.put('/', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.fileName));
@@ -64,11 +64,11 @@ router.put('/', async (req, res) => {
 
     res.send(account);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
-router.patch('/updateBalance', async (req, res) => {
+router.patch('/updateBalance', async (req, res, next) => {
   try {
     const account = req.body;
     const data = JSON.parse(await readFile(global.fileName));
@@ -79,7 +79,7 @@ router.patch('/updateBalance', async (req, res) => {
 
     res.send(data.accounts[index]);
   } catch (err) {
-    res.status(400).send({ error: err.message });
+    next(err);
   }
 });
 
