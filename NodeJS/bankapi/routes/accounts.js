d01cd9bc1cff9fb928ea0766c2821a0162ef1f5a
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-02-17 20:08:50 -0300

    Update Put/Patch

--- NodeJS/bankapi/routes/accounts.js
+++ NodeJS/bankapi/routes/accounts.js
@@ -53,4 +53,37 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
+router.put('/', async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex(acc => acc.id === account.id);
+
+    data.accounts[index] = account;
+    await writeFile(global.fileName, JSON.stringify(data));
+
+    res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.patch('/updateBalance', async (req, res) => {
+  try {
+    const account = req.body;
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex(acc => acc.id === account.id);
+
+    data.accounts[index].balance = account.balance;
+    await writeFile(global.fileName, JSON.stringify(data));
+
+    res.send(data.accounts[index]);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.use((err, req, res, next) => {
+  console.log(err);
+});
 export default router;
