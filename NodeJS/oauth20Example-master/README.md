f194a4889a87c19c285f656e558396127a7d5cd4
Henrique Alexandre <henriquealexandredec@gmail.com>
2023-03-31 00:29:11 -0300

    Salvando algumas tarefas do node.js

--- NodeJS/oauth20Example-master/README.md
+++
@@ -1,2 +0,0 @@
-# oauth20Example
-Exemplo utilizado para aula de Autenticação e Autorização no IGTI
--- NodeJS/oauth20Example-master/configuration/config.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    "api_key": "yourKey.apps.googleusercontent.com",
-    "api_secret": "secretKey",
-    "callback_url": "http://localhost:3000/auth/google/callback"
-}
